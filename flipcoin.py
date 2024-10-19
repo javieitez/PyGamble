316dcc2b794c9c9a435c59f2e160eb141f00b1a9
JA <joseantonio.vieitez@gmail.com>
2024-10-19 19:04:50 +0000

    refactor 2 function

--- flipcoin.py
+++ flipcoin.py
@@ -6,6 +6,7 @@ tailsCount = 0
 
 flipTimes = int(input('How many times do you want to flip the coin: '))
 
+# Flip the coin as much times as previously specified
 while flipTimes > 0:
     coinFlip = random.choice(coinSides)
     print(coinFlip)
@@ -15,14 +16,16 @@ while flipTimes > 0:
         tailsCount += 1 
     print(headsCount, 'heads,', tailsCount, 'tails')
     flipTimes -=1
+
+# Show some funny stats about the flips
 print('Coin was flipped', headsCount+tailsCount, 'times')
 
-if headsCount > tailsCount:
-    countDiff = headsCount-tailsCount
-    print(countDiff, 'more heads')
-    print(round(( 100 * countDiff )/headsCount), '% more heads')
-else:
-    countDiff = tailsCount-headsCount
-    print(countDiff, 'more tails')
-    print(round(( 100 * countDiff )/tailsCount), '% more tails')
+def showMeStats(a, b, whatMore):
+    countDiff = a-b
+    print(countDiff, 'more', whatMore)
+    print(round(( 100 * countDiff )/a), '% more', whatMore)
 
+if headsCount > tailsCount:
+    showMeStats(headsCount, tailsCount, 'heads')
+else:
+    showMeStats(tailsCount, headsCount, 'tails')

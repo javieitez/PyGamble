4bccba7d7b4a9f77a3fbdafa10f67137a134c5aa
JA <joseantonio.vieitez@gmail.com>
2024-10-20 16:55:29 +0000

    improed number

--- number.py
+++ number.py
@@ -1,6 +1,13 @@
 # enter 2 digits
-    firstNumber = int(input('Enter a Number: '))
-    secondNumber = int(input('Enter another Number: '))
+a = b = False
+while a == False:
+    c = input('Enter a Number: ')
+    firstNumber = int(c)
+    a = c.isdigit()
+while b == False:
+    c = input('Enter another Number: ')
+    secondNumber = int(c)
+    b = c.isdigit()
 
 #Compare which one is bigger
 if firstNumber > secondNumber:
@@ -23,4 +30,6 @@ print(firstNumber, 'times', secondNumber, 'is', (firstNumber*secondNumber))
 # Divide with decimals
 print(biggerNumber,'divided by', lesserNumber,'is', (biggerNumber / lesserNumber))
 
-
+# exponentiate
+print(biggerNumber, '**', lesserNumber, 'is', biggerNumber**lesserNumber)
+print('same as multiplying', lesserNumber, 'by itself', biggerNumber, 'times')

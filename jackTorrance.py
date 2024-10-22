f03f18251cf6456e2212e11fbbaaa9bc1cf06f59
JA <joseantonio.vieitez@gmail.com>
2024-10-22 18:21:48 +0000

    torrance-o-matic

--- jackTorrance.py
+++ jackTorrance.py
@@ -11,20 +11,20 @@ import time as t
 
 aa = 55
 interline = '*' * aa
-randCharz = ['*', '+', '-', '_', '¡']
+randCharz = ['*', '+', '-', '_', '¡', '#']
 
 # type one char at a time
 # type it at slightly different speeds to look a bit human
 def p(a):
     for char in a:
-        q = [.011, .066, .022, .055] 
+        q = [.011, .044, .022, .055] 
         rtime = r.choice(q)
         print(char, end='', flush=True)
         t.sleep(rtime)
 
 while x > 0:
     # generate random lines to choose from
-    myStylz= [w.capitalize(), w.lower(), w.upper(), w.rjust(aa), w.ljust(aa), 
+    myStylz= [w.capitalize(), w.lower(), w.upper(), w.rjust(aa), 
               w.center(aa), w.swapcase(), r.choice(randCharz) * aa, '\n']
     z = str(r.choice(myStylz)) # choose a random style
     aa = int(r.randint(30, 80)) # randomize the justification value

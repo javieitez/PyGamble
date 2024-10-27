456c2f97d1f38c567a182e1dcd6cea2f239eb93b
JA <joseantonio.vieitez@gmail.com>
2024-10-27 19:45:26 +0000

    guess the card extend

--- guessCard.py
+++ guessCard.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-#######################################################
-# guess the card game
-#######################################################
+#########################################################
+# guess the card game      ##############################
+# user will have three hints until the card is revealed #
+#########################################################
 
 import re, random, sys
 from termcolor import colored as c
@@ -15,32 +16,38 @@ clubs   = chr(9827)
 # init the cards deck
 suits = [hearts, diamonds, spades, clubs]
 cardNumbers = ['A', '2', '3', '4', '5', '6', '7', 'J', 'Q', 'K']
+suitColor = yourBetIcon = yourBetSuit = yourBetNumber = 'none yet'
+hitSuit = hitColor = hitNumber = False
 
 # pick a random card
 cardNumber = random.choice(cardNumbers)
 suit = random.choice(suits)
-yourBetIcon = yourBetSuit = yourBetNumber = 'none yet'
 
 def displayCard():
     rows = ['', '', '', '','']
-    rows[0] += ' ___ ' # top line of card
-    rows[1] += '|## |' 
-    rows[2] += '|###|'
-    rows[3] += '|_##|'
+    rows[0] += '    ___ ' # top line of card
+    rows[1] += '   |## |' 
+    rows[2] += '   |###|   GUESS THE CARD'
+    rows[3] += '   |_##|'
     rows[4] += ''
     for row in rows:
          print(c(row, 'green'))
 
-def revealCard():
+def setCardColor():
+    global suitColor
     if suit == hearts or suit == diamonds:
         suitColor = 'red'
     else:
         suitColor = 'cyan'
+
+setCardColor()
+
+def revealCard():
     rows = ['', '', '', '','']
-    rows[0] += ' ___  ' # top line of card
-    rows[1] += '|{} |'.format(c(cardNumber.ljust(2), suitColor))
-    rows[2] += '| {} |'.format(c(suit, suitColor))
-    rows[3] += '|_{}|'.format(c(cardNumber.rjust(2), suitColor))
+    rows[0] += '    ___  ' # top line of card
+    rows[1] += '   |{} |'.format(c(cardNumber.ljust(2), suitColor))
+    rows[2] += '   | {} |'.format(c(suit, suitColor))
+    rows[3] += '   |_{}|'.format(c(cardNumber.rjust(2), suitColor))
     rows[4] += ''
     for row in rows:
          print(row)
@@ -50,20 +57,21 @@ displayCard()
 # guess the card
 def makeYourBet():
     global yourBetSuit, yourBetNumber  
-    myRegex = '(A|J|Q|K|[2-7])'
+    myRegex = '^(A|J|Q|K|[2-7])$' # available inputs
     while not re.match(myRegex, yourBetNumber):
-        yourBetNumber = input('guess the card number: (A, 2 to 7, J, Q or K)')
-        yourBetNumber = yourBetNumber.upper() # go upppercase
-    myRegex = '(H|D|S|C)'
+        yourBetNumber = input('guess the card number: (A, 2 to 7, J, Q or K)').upper()
+    myRegex = '^(H|D|S|C)$' # available inputs 
     while not re.match(myRegex, yourBetSuit):
-        yourBetSuit = input('choose a suit: (H)earts, (D)iamonds, (S)pades or (C)lubs ')
-        yourBetSuit = yourBetSuit.upper() # go upppercase
+        yourBetSuit = input('choose a suit: (H)earts, (D)iamonds, (S)pades or (C)lubs ').upper()
+
+#debug
+print(cardNumber, suit)
 
 makeYourBet()
 
 # turn your keystrokes into readable words
 def yourBetWas():
-    global yourBetIcon
+    global yourBetIcon, yourBetSuit 
     if yourBetNumber == 'A':
         w = "Ace"
     elif yourBetNumber == 'J':
@@ -75,24 +83,90 @@ def yourBetWas():
     else:
         w = yourBetNumber
     if yourBetSuit == 'H':
-        a, b, yourBetIcon = 'Hearts', 'red', hearts 
+        a, b, yourBetIcon = 'Hearts', 'red', hearts
+        yourBetSuit = hearts
     elif yourBetSuit == 'D':
         a, b, yourBetIcon = 'Diamonds', 'red', diamonds
+        yourBetSuit = diamonds
     elif yourBetSuit == 'S':
         a, b, yourBetIcon = 'Spades', 'cyan', spades
+        yourBetSuit = spades
     elif yourBetSuit == 'C':
         a, b, yourBetIcon = 'Clubs', 'cyan', clubs
-    print('your bet was', c(w, b), 'of', c(a, b), c(yourBetIcon, b))
+        yourBetSuit = clubs
+    print('your bet was', c(w, b), 'of', 
+          c(a, b), c(yourBetIcon, b))
+
+# print hint function
+def ph(a, b, c):
+    myStr = a + ' ' + b + ' is ' + c
+    print(myStr)
+
+# convert ard string to int
+def convertCardNumber(i):
+    if i == 'A':
+        return 1
+    elif i == '2':
+        return 2
+    elif i == '3':
+        return 3
+    elif i == '4':
+        return 4
+    elif i == '5':
+        return 5
+    elif i == '6':
+        return 6
+    elif i == '7':
+        return 7
+    elif i == 'J':
+        return 10
+    elif i == 'Q':
+        return 11
+    elif i == 'K':
+        return 12
+
+# give the user a hint about hits and misses
+def giveHints():
+    if (yourBetSuit == diamonds or yourBetSuit == hearts) and suitColor == 'red':
+        ph('color', suitColor, 'right')
+        hitColor = True
+    elif (yourBetSuit == spades or yourBetSuit == clubs) and suitColor == 'cyan':
+        ph('color', suitColor, 'right')
+        hitColor = True
+    else:
+        if suitColor == 'cyan':
+            z = 'red'
+        else:
+            z = 'cyan'
+        ph('color', z, 'wrong')
+        hitColor = False
+    if yourBetSuit == suit:
+        ph('suit', suit, 'right')
+    else:
+        ph('suit', suit, 'wrong')
+    x = convertCardNumber(cardNumber)
+    y = convertCardNumber(yourBetNumber)
+    if x > y:
+        print('number is bigger than', y)
+    elif x < y:
+        print('number is smaller than', y)
+    else:
+        print('number is exactly', y)
 
 yourBetWas()
+
+giveHints()
 revealCard()
 # calculate results
 if yourBetNumber == cardNumber and yourBetIcon == suit:
-    print(c('You hitted both suit and number. you totally WON!!!', 'red', attrs=['blink']))
+    print(c('You hitted both suit and number. you totally WON!!! ', 
+            'green', attrs=['blink', 'reverse']))
 elif yourBetNumber == cardNumber: 
-    print(c('Not bad. You hitted the number and missed the suit', 'green', attrs=['blink']))
+    print(c('Not bad. You hitted the number and missed the suit. ', 
+            'green', attrs=['reverse']))
 elif yourBetIcon == suit:
-    print(c('Not bad. You hitted the suit and missed the number', 'green', attrs=['blink']))
+    print(c('Not bad. You hitted the suit and missed the number. ', 
+            'green', attrs=['reverse']))
 else:
     print("You missed both suit and number. Not even close")
 
